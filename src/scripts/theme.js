7bfa8482f7aef768a284d0921c6dda02050c5260
BJ Orbase <bjorbase@brandedonline.com>
2018-08-31 15:50:17 -0400

    JS : Removed extraneous context variable

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -996,8 +996,6 @@ theme.Newsletter = (function() {
     // regex for valid email 
     
     const regexEmail = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i);
-    const self = this;
-
 
     if ( ui.formId ) {
 
