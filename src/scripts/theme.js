c9c0cc2b88a7161c056317c6f52a4c7cc95805bd
Priya Gohil <pgohil@brandedonline.com>
2018-10-03 10:10:15 -0700

    MUCK-18 : FAQ removed script from theme.js to page.faq.liquid

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1653,19 +1653,6 @@ $(document).ready(function() {
       }
     });
 
-    /* For FAQ */
-    if( $(".toggle .toggle-title").hasClass('active') ){
-      $(".toggle .toggle-title.active").closest('.toggle').find('.toggle-inner').show();
-    }
-  
-    $(".toggle .toggle-title").click(function(){
-      if( $(this).hasClass('active') ){
-        $(this).removeClass("active").closest('.toggle').find('.toggle-inner').slideUp(200);
-      }
-      else{ 
-        $(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
-      }
-    });
 
     /*============================================================================
     Shopify Section Load
