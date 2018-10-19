f7f27c1c97526375d3c11d0467b1418c3b442021
Brad McGlasson <brad.mcglasson@onestop.com>
2018-10-19 12:39:20 -0700

    IE-FIX : JS (Theme) : Polyfill for using element.closest() which is for swatch switching on PDP

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -651,6 +651,27 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 }(jQuery, window, document));
 
 
+/*
+ * ----------------------------------------------------------------------------------------------------
+ * ELEMENT.CLOSEST() POLYFILL -- This fixes IE 11 bug when changing swatches and others
+ * ----------------------------------------------------------------------------------------------------
+ */ 
+if (!Element.prototype.matches)
+    Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                                Element.prototype.webkitMatchesSelector;
+
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        var el = this;
+        if (!document.documentElement.contains(el)) return null;
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1); 
+        return null;
+    };
+}
+
  /**
  * ----------------------------------------------------------------------------------------------------
  * STICKY NAV
