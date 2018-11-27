f97c3ae7d972ce0802c934a2b3f7318084f10d78
Brad McGlasson <brad.mcglasson@onestop.com>
2018-11-26 20:51:18 -0800

    REACT-DISCOUNTS : LIQUID + PARENT : Refactor liquid and parent code to respect enable flags as well as pass in config data through the component snippet

--- src/scripts/react-components/discounts/DiscountParent.js
+++ src/scripts/react-components/discounts/DiscountParent.js
@@ -2,12 +2,12 @@
  * DISCOUNTS : REACT
  *  - Handles discounts based on things like...
  *      "Spend $___ and get Product Y for Free / Discounted" (currently)
- *      "Buy 4 items, pick free gift from list" (Stretch goal)
+ *      "Spend $___ pick from set of products" 
  *
  *  - DOES NOT handle Coupon Code stuff, "Buy 2 get 1 free" are done via shopify scripts (For now)
  *
  * REQUIRED DATA:
- *  - Root EL with ID "react-discounts" in root of theme layout
+ *  - Root EL with ID "react-discounts" in root of theme layout and a base64, stringified config object on data-discount-config prop
  *
  *
  * OPTIONAL DATA:
@@ -20,16 +20,32 @@ const DiscountManager = require('./DiscountManager.js');
 // PARENT : Initialize discounts manager + Render into Root el
 $(document).ready( function(){
   
-  // SAFETY : I can haz DOM Node?
+  // SAFETY : I can haz DOM Node + Config settings?
   const rootEl = document.getElementById('react-discounts');
-  if ( rootEl ) {
-    console.log( 'found render target for parent...' );
 
-    // RENDER : Use ReactDOM to render DiscountManager into the div
-    ReactDOM.render(
-      <DiscountManager />,
-      rootEl
-    );
+  // SAFETY : DOM Node + Proper Config
+  if ( rootEl ) {
+    const discountConfig = rootEl.dataset.discountConfig;
+    var parsedConfig = null;
+
+    // SAFETY : Attempt to parse config from dom data
+    if ( discountConfig && discountConfig.length > 0 && window.atob ) {
+      try {
+        parsedConfig = JSON.parse( window.atob( discountConfig ) );
+      }
+
+      catch( err ) {
+        console.log( `[ DiscountParent -- init() ] ERROR : Looks like the discountConfig was not parsable, printout below:\n${discountConfig}` );
+      }
+    }
+
+    // RENDER : If we have a config and a DOM Node, lets render the component!
+    if ( parsedConfig && parsedConfig.cookieExpireInDays ) {
+      ReactDOM.render(
+        <DiscountManager config={ parsedConfig }/>,
+        rootEl
+      );
+    }
   
   // SAFETY : No DOM Node : Report node is missing, but script was invoked still
   } else {
