78c9c4b982c3408097d61e28b64e93d57482baf5
nilay.pithaidya <nilaypshivlab>
2019-08-01 10:35:15 +0530

    Yotpo PLP Updates

--- src/assets/bc-sf-filter.js
+++ src/assets/bc-sf-filter.js
@@ -34,6 +34,7 @@ var bcSfFilterTemplate = {
                                         '{{itemVendor}}' +
                                         '<h3 class="product-title">{{itemTitle}}</h3>' +
                                     '</a>' +
+                                    '<div class="yotpo bottomLine" data-product-id="{{itemProductId}}"></div>'+
                                     '<div class="product-price-wrap">{{itemPrice}}</div>' +
                                 '</div>' +
                                 '{{itemQuickview}}' +
@@ -60,6 +61,7 @@ var bcSfFilterTemplate = {
 
 
 BCSfFilter.prototype.buildProductGridItem = function(data, index, totalProduct) {
+    window.total_display_product = totalProduct;
     /*** Prepare data ***/
     var images = data.images_info;
     
@@ -282,6 +284,11 @@ BCSfFilter.prototype.buildProductGridItem = function(data, index, totalProduct) 
 
 // Build Pagination
 BCSfFilter.prototype.buildPagination = function(totalProduct) {
+    if (typeof yotpo !== 'undefined') {
+        //check yopto is enable or not
+        yotpo.initWidgets();
+        window.display_product = true;
+    }
     // Get page info
     var currentPage = parseInt(this.queryParams.page);
     var totalPage = Math.ceil(totalProduct / this.queryParams.limit);
